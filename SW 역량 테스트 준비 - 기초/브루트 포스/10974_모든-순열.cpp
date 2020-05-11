9173e2a70eedf680b7f08222c2b03619664460ec
David Park <dpjungmin@gmail.com>
2020-05-11 13:38:41 +0900

    Update solution

--- SW 역량 테스트 준비 - 기초/브루트 포스/10974_모든-순열.cpp
+++ SW 역량 테스트 준비 - 기초/브루트 포스/10974_모든-순열.cpp
@@ -46,3 +46,32 @@ int main()
     input();
     dfs(0);
 }
+
+// Solution using next_permutation()
+
+// #include <bits/stdc++.h>
+
+// #define endl "\n"
+// #define fastIO                   \
+//     ios::sync_with_stdio(false); \
+//     cin.tie(NULL)
+
+// using namespace std;
+
+// int main()
+// {
+//     fastIO;
+//     int n;
+//     cin >> n;
+
+//     vector<int> v(n);
+
+//     for (int i = 0; i < n; i++)
+//         v[i] = i + 1;
+
+//     do {
+//         for (int i = 0; i < n; i++)
+//             cout << v[i] << ' ';
+//         cout << endl;
+//     } while (next_permutation(begin(v), end(v)));
+// }
