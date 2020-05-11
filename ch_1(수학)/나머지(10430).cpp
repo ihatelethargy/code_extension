2da86d28ed673311b2e7e1ebd7f88a6f3edb4b46
ihatelethargy <naturalflavor1007@naver.com>
2020-05-11 21:47:07 +0900

    나머지

--- ch_1(수학)/나머지(10430).cpp
+++ ch_1(수학)/나머지(10430).cpp
@@ -2,3 +2,25 @@
 #include <algorithm>
 using namespace std;
 
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	
+	int line1 = (a + b) % c;
+	int line2 = (a % c + b % c) % c;
+	int line3 = (a * b) % c;
+	int line4 = (a % c * b % c) % c;
+	
+	
+	cout << line1 << endl
+		<< line1 << endl
+		<< line3 << endl
+		<< line4 << endl;
+}
+
+/*
+
+line1 == line2
+line3 == line4
+
+*/
