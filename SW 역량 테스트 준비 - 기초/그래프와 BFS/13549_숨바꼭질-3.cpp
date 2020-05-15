7cbfd2dd634e077e55efc2e650edd1d9773e9f91
David Park <dpjungmin@gmail.com>
2020-05-15 23:31:04 +0900

    Add solution

---
+++ SW 역량 테스트 준비 - 기초/그래프와 BFS/13549_숨바꼭질-3.cpp
@@ -0,0 +1,64 @@
+// queue 1개로도 풀 수 있는 문제
+// bfs 수행시 순간이동과 앞, 뒤 이동이 충돌하지 않음
+
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+const int MAX = 100001;
+
+int n, k;
+bool vis[MAX];
+deque<pair<int, int>> dq;
+
+void input()
+{
+    cin >> n >> k;
+}
+
+void solve()
+{
+    vis[n] = true;
+    int ans(-1);
+    dq.push_back({n, 0});
+
+    while (!dq.empty())
+    {
+        auto [pos, time] = dq.front();
+        dq.pop_front();
+
+        if (pos == k)
+        {
+            ans = time;
+            break;
+        }
+
+        if (pos * 2 < MAX && !vis[pos * 2])
+        {
+            vis[pos * 2] = true;
+            dq.push_front({pos * 2, time});
+        }
+
+        if (pos - 1 >= 0 && !vis[pos - 1])
+        {
+            vis[pos - 1] = true;
+            dq.push_back({pos - 1, time + 1});
+        }
+
+        if (pos + 1 < MAX && !vis[pos + 1])
+        {
+            vis[pos + 1] = true;
+            dq.push_back({pos + 1, time + 1});
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    input();
+    solve();
+}
